af52fb8e1a6396a6005656d03bcc202ad7d1b226
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-15 13:24:27 +0100

    import BookList into app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import BookList from "./components/BookList";
 
 const App = () => {
   return (
     <div className="app">
       <h1>Avi's Reading List</h1>
+      <BookList />
     </div>
   );
 }
