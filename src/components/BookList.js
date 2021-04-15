e0d05b4345446dfce2da6d4956ed0d668b1c1acc
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-15 17:10:36 +0100

    make a graphql query to fetch all books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,38 @@
 import React from "react";
+// import apollo hooks and methods
+import { useQuery, gql } from '@apollo/client';
+
+// create a new query (get all books in this instance)
+const GET_ALL_BOOKS = gql`
+    {
+        books {
+            title
+            genre
+            author{
+                name
+            }
+        }  
+    }
+`;
 
 const BookList = () => {
+
+    const { loading, error, data } = useQuery(GET_ALL_BOOKS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
     return (
         <div>
             <ul className="book-list">
-                <li>Books...</li>
+                { data.books.map((book, index) => (
+                    <li key={index}>
+                        <h3>{book.title}</h3>
+                        <p>{`Genre: ${book.genre}`}</p>
+                        <p>{`Author: ${book.author.name}`}</p>
+                        <br/>
+                    </li>
+                )) }
             </ul>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache()
 });
-
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>,
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
